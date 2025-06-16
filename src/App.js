cfd0e60a9e9a9f21ea7f8c96fd3afbb22730c73e
Fatimah Sharafudeen <fatimah.sharafudeen@gmail.com>
2025-06-16 17:32:31 +0100

    Created a new component called Weather

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Weather from './Weather';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>HELLO</h1>
+      <h1 className='btn btn-primary'>HELLO</h1>
+      <Weather/>
     </div>
   );
 }
 
-export default App;
+ 
