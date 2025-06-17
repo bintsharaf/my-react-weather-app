398ffe301db1115f43e7c584a8f611219b0d6319
Fatimah Sharafudeen <fatimah.sharafudeen@gmail.com>
2025-06-17 09:54:37 +0100

    Created and updated the html file of the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import Weather from './Weather';
 
 export default function App() {
   return (
+    <div className='container'> 
     <div className="App">
-      <h1 className='btn btn-primary'>HELLO</h1>
       <Weather/>
+      <footer>
+
+      </footer>
+    </div>
     </div>
   );
 }
