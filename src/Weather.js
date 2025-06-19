07c55c601dbfbf5b91c43ab572ac1f6662c732ad
Fatimah Sharafudeen <fatimah.sharafudeen@gmail.com>
2025-06-19 21:18:19 +0100

    Updated the interactivity of the weather app project

--- src/Weather.js
+++ src/Weather.js
@@ -1,40 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css"; 
 export default function Weather() {
-    return (
+    const apiKey = "29583e5b03o3adtc2486edaf9f3af0e3";
+    const [city, setCity] = useState("");
+    const [loaded, setLoaded] = useState(false);
+    const [weatherData, setWeatherData] = useState({});
+    
+
+    function displayWeather(response) {
+        setLoaded(true);
+        setWeatherData({
+            city: response.data.city,
+            temperature: Math.round(response.data.temperature.current),
+            description: response.data.condition.description,
+            icon: response.data.condition.icon_url,
+            humidity: response.data.temperature.humidity,
+            wind: Math.round(response.data.wind.speed),
+            precipitation: response.data.precipitation?.value || 0,
+
+        });
+    }
+
+    function updateCity(event) {
+        setCity(event.target.value)
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(displayWeather);
+    }
+    if (loaded) {
+        return (
         <div className="Weather">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="row">
                     <div className="col-9">
-                        <input type="search" placeholder="Enter a City..." className="form-control" autoFocus="on"/>
+                        <input type="search" placeholder="Enter a City..." className="form-control" onChange={updateCity} autoFocus="on"/>
                     </div>
                     <div className="col-3">
                         <input type="submit" value="Search" className="btn btn-primary w-100"/>
                     </div>  
                 </div>
             </form>
-            <h1>Lagos</h1>
+            <h1>{weatherData.city}</h1>
             <ul>
                 <li>Wednesday</li>
-                <li>Mostly cloudy</li>
+                <li>{weatherData.description}</li>
             </ul>
             <div className="row">
                 <div className="col-6">
                     <div className="clear-fix">
 
                     </div>
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="mostly cloudy" className="float-left"/>
-                    <span className="temperature">27</span>
+                    <img src= {weatherData.icon} alt= {weatherData.description} className="float-left"/>
+                    <span className="temperature">{weatherData.temperature}</span>
                     <span className="unit">°C</span>
                 </div>
                 <div className="col-6">
                     <ul>
-                        <li>Precipitation: 0%</li>
-                        <li>Humidity: 86%</li>
-                        <li>Wind: 8 km/h</li>
+                        <li>Precipitation: {weatherData.precipitation}%</li>
+                        <li>Humidity: {weatherData.humidity}%</li>
+                        <li>Wind: {weatherData.wind} km/h</li>
                     </ul>
                 </div>
             </div>
         </div>
     )
+    } else {
+        return (
+            <form onSubmit={handleSubmit}>
+                <div className="row">
+                    <div className="col-9">
+                        <input type="search" placeholder="Enter a City..." className="form-control" onChange={updateCity} autoFocus="on"/>
+                    </div>
+                    <div className="col-3">
+                        <input type="submit" value="Search" className="btn btn-primary w-100"/>
+                    </div>  
+                </div>
+            </form>
+        )
+    }
+    
 }
