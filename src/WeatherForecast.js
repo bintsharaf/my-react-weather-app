3ad48bcede459cf28414daca495b9de408f0e961
Fatimah Sharafudeen <fatimah.sharafudeen@gmail.com>
2025-06-21 23:01:48 +0100

    Created a day forecast i.e. 1 day

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,27 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
 import axios from "axios";
 export default function WeatherForecast(props) {
+    let [loaded, setLoaded] = useState(false);
+    let [forecast, setForecast] =useState(null);
     function handleResponse(response) {
-        console.log(response.data);
+        setForecast(response.data.daily);
+        setLoaded(true);
     }
-    let apiKey = "29583e5b03o3adtc2486edaf9f3af0e3";
-    let latitude = props.data.latitude;
-    let longitude = props.data.longitude;
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`
-    axios.get(apiUrl).then(handleResponse);
-    return (
+    
+    if (loaded) {
+            return (
         <div className="WeatherForecast">
             <div className="row">
                 <div className="col">
-                    <div className="WeatherForecast-Day">Thu</div>
-                    <div className="WeatherForecast-icon"><img src= {props.data.icon} alt= {props.data.description}/></div>
-                    <div className="WeatherForecast-temperatures">
-                        <span className="WeatherForecast-temperature-max">19°</span>
-                        <span className="WeatherForecast-temperature-min">10°</span>
-                    </div>
+                    <WeatherForecastDay data ={forecast[0]}/>
                 </div>
             </div>
         </div>
     )
+    } else {
+        let apiKey = "29583e5b03o3adtc2486edaf9f3af0e3";
+        let latitude = props.data.latitude;
+        let longitude = props.data.longitude;
+        let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`
+        axios.get(apiUrl).then(handleResponse);
+    }
+
 }
