a776d4a4c06ef8ea4d7e3323046222701f37eadb
Fatimah Sharafudeen <fatimah.sharafudeen@gmail.com>
2025-06-24 18:22:56 +0100

    Updated the app footer

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
     <div className="container">
       <Weather/>
       <footer>
-          This project was coded by <a href='http://www.linkedin.com/in/fatimahsharafudeen'>Fatimah Sharaf</a> and it is open - sourced on <a href='/'>GitHub</a> and hosted on <a href='https://courageous-cobbler-cfd88e.netlify.app/'>Netlify</a> 
+          This project was coded by <a href='http://www.linkedin.com/in/fatimahsharafudeen'>Fatimah Sharaf</a> and it is open - sourced on <a href='https://github.com/bintsharaf/'>GitHub</a> and hosted on <a href='https://courageous-cobbler-cfd88e.netlify.app/'>Netlify</a> 
       </footer>
     </div>
     </div>
